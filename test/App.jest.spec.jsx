7130a40f1d37d18a329a7e70b664f625554cb55a
IanTsai1 <113403773+IanTsai1@users.noreply.github.com>
2025-01-28 16:16:25 +0800

    Performed prettier and lint and added a github action

--- test/App.jest.spec.jsx
+++ test/App.jest.spec.jsx
@@ -1,35 +1,54 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import axiosMock from 'axios'
-import { act } from 'react-dom/test-utils'
-import '@testing-library/jest-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from '../src/App'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import axiosMock from "axios";
+import { act } from "react-dom/test-utils";
+import "@testing-library/jest-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "../src/App";
 
-jest.mock('axios')
+jest.mock("axios");
 
-describe('<App />', () => {
-  it('fetches data', async () => {
-    axiosMock.get.mockResolvedValueOnce(
-      {
-        data: {
-          results: [{ url: 'https://pokeapi.co/api/v2/pokemon/1/', name: 'bulbasaur', id: 1 }]
-        }
-      }
-    )
+describe("<App />", () => {
+  it("fetches data", async () => {
+    // simulates an API call to get data for Bulbasaur
+    axiosMock.get.mockResolvedValueOnce({
+      data: {
+        results: [
+          {
+            url: "https://pokeapi.co/api/v2/pokemon/1/",
+            name: "bulbasaur",
+            id: 1,
+          },
+        ],
+      },
+    });
+    // by adding await act, we ensure this is fully executed before we make assertions
+    // like how many api calls is made and the api url
     await act(async () => {
-      render(<Router><App/></Router>)
-    })
-    expect(axiosMock.get).toHaveBeenCalledTimes(1)
-    expect(axiosMock.get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/?limit=50')
-  })
+      render(
+        <Router>
+          <App />
+        </Router>,
+      );
+    });
+    // make sure the api is only called once
+    // make sure api url is correct
+    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+    expect(axiosMock.get).toHaveBeenCalledWith(
+      "https://pokeapi.co/api/v2/pokemon/?limit=50",
+    );
+  });
 
-  it('shows error', async () => {
-    axiosMock.get.mockRejectedValueOnce(new Error())
+  // if there's an error, it gracefully handles the error by informing the user
+  it("shows error", async () => {
+    axiosMock.get.mockRejectedValueOnce(new Error());
     await act(async () => {
-      render(<Router><App/></Router>)
-    })
-    expect(screen.getByTestId('error')).toBeVisible()
-  })
-
-})
+      render(
+        <Router>
+          <App />
+        </Router>,
+      );
+    });
+    expect(screen.getByTestId("error")).toBeVisible();
+  });
+});
--- test/PokemonList.jest.spec.jsx
+++ test/PokemonList.jest.spec.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import { BrowserRouter } from 'react-router-dom'
-import '@testing-library/jest-dom'
-import PokemonList from '../src/PokemonList'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+import PokemonList from "../src/PokemonList";
 
+const pokemonList = [
+  {
+    url: "https://pokeapi.co/api/v2/pokemon/1/",
+    name: "bulbasaur",
+    id: 1,
+  },
+  {
+    url: "https://pokeapi.co/api/v2/pokemon/133/",
+    name: "eevee",
+    id: 133,
+  },
+];
 
-const pokemonList = [{
-  url: 'https://pokeapi.co/api/v2/pokemon/1/',
-  name: 'bulbasaur',
-  id: 1
-}, {
-  url: 'https://pokeapi.co/api/v2/pokemon/133/',
-  name: 'eevee',
-  id: 133
-}]
-
-describe('<PokemonList />', () => {
-  it('should render items', () => {
+// given the pokemon list above, the PokemonList component should
+// render bulbasaur and evee in the screen; check if everythign is displayed correctly
+describe("<PokemonList />", () => {
+  it("should render items", () => {
     render(
       <BrowserRouter>
         <PokemonList pokemonList={pokemonList} />
-      </BrowserRouter>
-    )
-    expect(screen.getByText('bulbasaur')).toBeVisible()
-    expect(screen.getByText('eevee')).toBeVisible()
+      </BrowserRouter>,
+    );
+
+    // check these two pokemon name text are visible on screen
+    expect(screen.getByText("bulbasaur")).toBeVisible();
+    expect(screen.getByText("eevee")).toBeVisible();
   })
-})
+});
