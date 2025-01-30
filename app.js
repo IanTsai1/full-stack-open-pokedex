9849fe49de130f7cda833dde955c6a984848f3d1
IanTsai1 <113403773+IanTsai1@users.noreply.github.com>
2025-01-30 14:49:02 +0800

    Added health checker to deployment

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
+app.get("/health", (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw new Error("error..."); // Use conditional logic for error handling if needed
+  res.send("ok");
+});
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
